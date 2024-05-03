81204a81e47b3184273dc0394bc828224487a399
Emon-Ahmed <technicalboyemon@gmail.com>
2024-05-04 01:25:05 +0600

    New Files Created

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main>
       <h1>Hello Emon</h1>
     </main>
   );
